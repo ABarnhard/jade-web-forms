d7b5375ab2401a432add133b0c0949c5cf4867a3
Adam Barnhard <admbarn@gmail.com>
2014-07-31 12:19:02 -0500

    finished calc

--- app/index.js
+++ app/index.js
@@ -3,15 +3,44 @@
 var express = require('express');
 var app = express();
 var morgan  = require('morgan');
+var bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
   res.render('index');
+});
+
+app.get('/calc', function(req, res){
+  res.render('calc');
+});
+
+app.post('/calc', function(req, res){
+  var x = req.body.x * 1, y = req.body.y * 1, result, opN, eql = ' = ';
+  switch(req.body.op){
+    case '+':
+      result = x + y;
+      opN = 'Sum ';
+      break;
+    case '-':
+      result = x - y;
+      opN = 'Subtract ';
+      break;
+    case '*':
+      result = x * y;
+      opN = 'Multiply ';
+      break;
+  case '/':
+      result = x / y;
+      opN = 'Divide ';
+  }
+
+  res.render('calc', {eql:eql,opN:opN,op:' '+req.body.op+' ',res:result, x:req.body.x, y:req.body.y});
 });
 
 var port = process.env.PORT;
