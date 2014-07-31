a5bbf6e8ecf2e9b1602bbbbebfe148d4a8aa2fee
Adam Barnhard <admbarn@gmail.com>
2014-07-31 14:46:27 -0500

    finished boxes

--- app/index.js
+++ app/index.js
@@ -43,6 +43,33 @@ app.post('/calc', function(req, res){
   res.render('calc', {eql:eql,opN:opN,op:' '+req.body.op+' ',res:result, x:req.body.x, y:req.body.y});
 });
 
+app.get('/boxes', function(req, res){
+  res.render('box1');
+});
+
+app.post('/boxes', function(req, res){
+  var numBox = parseInt(req.body.box);
+  var widths = req.body.width.split('-').map(function(x){return x * 1;});
+  var heights = req.body.height.split('-').map(function(x){return x * 1;});
+  var colors = req.body.color.split(',');
+
+  console.log(numBox,widths,heights,colors);
+
+  var boxes = [], index;
+
+  for(var i = 0; i < numBox; i++){
+    var box = {};
+    box.width = Math.floor(Math.random() * (widths[1] - widths[0] + 1)) + widths[0];
+    box.height = Math.floor(Math.random() * (heights[1] - heights[0] + 1)) + heights[0];
+    index = Math.floor(Math.random() * colors.length);
+    box.color = colors[index];
+
+    boxes.push(box);
+  }
+
+  res.render('box2', {boxes:boxes});
+});
+
 var port = process.env.PORT;
 
 app.listen(port, function(){
